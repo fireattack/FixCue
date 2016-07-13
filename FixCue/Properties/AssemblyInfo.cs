a20c571937c9d9f1ea85bd4b367b25a6517f1250
fireattack <human.peng@gmail.com>
2016-07-12 22:59:58 -0500

    Update version

--- FixCue/Properties/AssemblyInfo.cs
+++ FixCue/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      内部版本号
 //      修订号
 //
-[assembly: AssemblyVersion("1.4.3.0")]
-[assembly: AssemblyFileVersion("1.4.3.0")]
+[assembly: AssemblyVersion("1.4.4.0")]
+[assembly: AssemblyFileVersion("1.4.4.0")]
